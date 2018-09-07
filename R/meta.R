c9d379a8d2e8cee49567dc3abfa4ea2bd89ada6c
ThierryO <thierry.onkelinx@inbo.be>
2018-09-07 10:24:13 +0200

    add meta()

---
+++ R/meta.R
@@ -0,0 +1,132 @@
+#' Optimize a vector for storage as plain text and add meta data
+#' @param x the vector
+#' @param optimize recode the data to get smaller text files. Defaults to TRUE
+#' @name meta
+#' @rdname meta
+#' @exportMethod meta
+#' @docType methods
+#' @importFrom methods setGeneric
+setGeneric(
+  name = "meta",
+  def = function(x, optimize = TRUE){
+    standardGeneric("meta") # nocov
+  }
+)
+
+#' @rdname meta
+#' @importFrom methods setMethod
+setMethod(
+  f = "meta",
+  signature = signature(x = "character"),
+  definition = function(x, optimize = TRUE){
+    attr(x, "meta") <- "    class: character"
+    if (any(is.na(x))) {
+        stop(
+            call. = FALSE,
+"The string 'NA' cannot be stored because it would be indistinguishable from the
+missing value NA. Please replace or remove any 'NA' strings. Consider using a
+factor."
+        )
+    }
+    return(x)
+  }
+)
+
+#' @rdname meta
+#' @importFrom methods setMethod
+setMethod(
+  f = "meta",
+  signature = signature(x = "integer"),
+  definition = function(x, optimize = TRUE){
+    attr(x, "meta") <- "    class: integer"
+    return(x)
+  }
+)
+
+#' @rdname meta
+#' @importFrom methods setMethod
+setMethod(
+  f = "meta",
+  signature = signature(x = "numeric"),
+  definition = function(x, optimize = TRUE){
+    attr(x, "meta") <- "    class: numeric"
+    return(x)
+  }
+)
+
+#' @rdname meta
+#' @importFrom methods setMethod
+setMethod(
+  f = "meta",
+  signature = signature(x = "factor"),
+  definition = function(x, optimize = TRUE){
+    if (isTRUE(optimize)) {
+        z <- as.integer(x)
+    } else {
+        z <- x
+    }
+    attr(z, "meta") <- paste(
+        "    class: factor\n    levels:",
+        paste("        -", levels(x), collapse = "\n"),
+        sep = "\n"
+    )
+    return(z)
+  }
+)
+
+#' @rdname meta
+#' @importFrom methods setMethod
+setMethod(
+  f = "meta",
+  signature = signature(x = "logical"),
+  definition = function(x, optimize = TRUE){
+    if (isTRUE(optimize)) {
+        x <- as.integer(x)
+    }
+    attr(x, "meta") <- "    class: logical"
+    return(x)
+  }
+)
+
+#' @rdname meta
+#' @importFrom methods setMethod
+setMethod(
+  f = "meta",
+  signature = signature(x = "complex"),
+  definition = function(x, optimize = TRUE){
+    attr(x, "meta") <- "    class: complex"
+    return(x)
+  }
+)
+
+#' @rdname meta
+#' @importFrom methods setMethod
+setMethod(
+  f = "meta",
+  signature = signature(x = "POSIXct"),
+  definition = function(x, optimize = TRUE){
+    if (isTRUE(optimize)) {
+        z <- unclass(x)
+    } else {
+        z <- x
+    }
+    attr(z, "meta") <- "    class: POSIXct\n    origin: 1970-01-01\n"
+    return(z)
+  }
+)
+
+#' @rdname meta
+#' @importFrom methods setMethod
+setMethod(
+  f = "meta",
+  signature = signature(x = "Date"),
+  definition = function(x, optimize = TRUE){
+    if (isTRUE(optimize)) {
+        z <- unclass(x)
+    } else {
+        z <- x
+    }
+    attr(z, "meta") <- "    class: Date\n    origin: 1970-01-01\n"
+    return(z)
+  }
+)
