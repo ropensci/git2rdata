601e2e2b993f2a444028bb96a6ec95d28ab47791
ThierryO <thierry.onkelinx@inbo.be>
2018-09-08 15:53:42 +0200

    add recent_commit()

---
+++ R/recent_commit.R
@@ -0,0 +1,42 @@
+#' Most recent file change
+#' Retrieve the most recent commit in which a file or data object existed.
+#' @inheritParams write_vc
+#' @param data refers file to a file (FALSE) or a data object (TRUE). Defaults to FALSE
+#' @rdname recent_commit
+#' @exportMethod recent_commit
+#' @docType methods
+#' @importFrom methods setGeneric
+setGeneric(
+  name = "recent_commit",
+  def = function(file, root, data = FALSE){
+    standardGeneric("recent_commit") # nocov
+  }
+)
+
+#' @rdname recent_commit
+#' @importFrom methods setMethod
+#' @importFrom assertthat assert_that is.string is.flag
+#' @importFrom git2r odb_blobs
+setMethod(
+  f = "recent_commit",
+  signature = signature(root = "git_repository"),
+  definition = function(file, root, data = FALSE){
+    assert_that(is.string(file))
+    assert_that(is.flag(data))
+
+    if (data) {
+      file <- clean_data_path(root = ".", file, normalize = FALSE)
+    }
+    name <- basename(file)
+    path <- gsub("^\\./?", "", unique(dirname(file)))
+    blobs <- odb_blobs(root)
+    blobs <- blobs[blobs$path == path & blobs$name %in% name, ]
+    blobs <- blobs[blobs$when == max(blobs$when), c("commit", "author", "when")]
+    blobs <- unique(blobs)
+    if (nrow(blobs) > 1) {
+        warning("Multiple commits within the same second")
+    }
+    rownames(blobs) <- NULL
+    blobs
+  }
+)
---
+++ tests/testthat/test_d_recent_commit.R
@@ -0,0 +1,56 @@
+context("recent_commit")
+
+# currently odb_blobs() can't handle subsecond commits
+# when TRUE Sys.sleep(1.1) is added before each commit
+subsecond <- TRUE
+
+root <- tempfile(pattern = "git2rdata-recent")
+dir.create(root)
+root <- git2r::init(root)
+git2r::config(root, user.name = "Alice", user.email = "alice@example.org")
+
+write_vc(test_data[1:2, ], file = "test1", root = root, stage = TRUE)
+commit_1 <- git2r::commit(root, "initial commit")
+
+write_vc(test_data[3:4, ], file = "junk/test1", root = root, stage = TRUE)
+commit_2 <- git2r::commit(root, "second file")
+
+write_vc(test_data[5:6, ], file = "test1", root = root, stage = TRUE)
+if (subsecond) Sys.sleep(1.1)
+commit_3 <- git2r::commit(root, "update first file")
+
+write_vc(test_data[7:8, ], file = "test3", root = root, stage = TRUE)
+if (subsecond) Sys.sleep(1.1)
+commit_4 <- git2r::commit(root, "add third file")
+
+write_vc(test_data[9:10, ], file = "test3", root = root, stage = TRUE)
+commit_5 <- git2r::commit(root, "update third file")
+
+expect_identical(
+  recent_commit(file = "test1.tsv", root),
+  data.frame(
+    commit = commit_3$sha,
+    author = commit_3$author$name,
+    when = as.POSIXct(commit_3$author$when),
+    stringsAsFactors = FALSE
+  )
+)
+expect_identical(
+  recent_commit(file = "junk/test1", root, data = TRUE),
+  data.frame(
+    commit = commit_2$sha,
+    author = commit_2$author$name,
+    when = as.POSIXct(commit_2$author$when),
+    stringsAsFactors = FALSE
+  )
+)
+
+write_vc(test_data[11:12, ], file = "subsecond", root = root, stage = TRUE)
+commit_6 <- git2r::commit(root, "first subsecond")
+write_vc(test_data[13:14, ], file = "subsecond", root = root, stage = TRUE)
+commit_7 <- git2r::commit(root, "second subsecond")
+expect_warning(
+  output <- recent_commit(file = "subsecond", root, data = TRUE),
+  "Multiple commits within the same second"
+)
+expect_true(all(output$commit %in% c(commit_6$sha, commit_7$sha)))
