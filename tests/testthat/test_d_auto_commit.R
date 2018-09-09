20a29a5c979a1d94bd55fc2656828959d36eebeb
ThierryO <thierry.onkelinx@inbo.be>
2018-09-09 23:50:59 +0200

    add auto_commit()

---
+++ tests/testthat/test_d_auto_commit.R
@@ -0,0 +1,52 @@
+context("autocommit changes")
+# create test repository
+origin.path <- tempfile(pattern = "git2r-")
+connection <- tempfile(pattern = "git2rclone-")
+dir.create(origin.path)
+dir.create(connection)
+repo_bare <- git2r::init(origin.path, bare = TRUE)
+repo <- git2r::clone(origin.path, connection, progress = FALSE)
+git2r::config(repo, user.name = "Alice", user.email = "alice@example.org")
+
+write_vc(
+  test_data, file = "test1", root = repo, sorting = "test_Date", stage = TRUE
+)
+init <- commit(repo, "inital")
+push(repo, "origin", "refs/heads/master")
+
+expect_identical(
+  auto_commit(package = "git2rdata", repo = repo),
+  init
+)
+write_vc(
+  test_subset, file = "test1", root = repo, sorting = "test_Date", stage = TRUE
+)
+expect_is(
+  cm <- auto_commit(package = "git2rdata", repo = repo),
+  "git_commit"
+)
+expect_match(cm$message, "Scripted commit from git2rdata")
+
+write_vc(test_data, file = "test1", root = repo, stage = TRUE)
+expect_message(
+  cm <- auto_commit(package = "git2rdata", repo = connection, push = TRUE),
+  "Pushing changes to remote repository"
+)
+expect_identical(cm, git2r::last_commit(repo))
+
+file.remove(list.files(origin.path, recursive = TRUE, full.names = TRUE))
+write_vc(test_subset, file = "test1", root = repo, stage = TRUE)
+expect_warning(
+  cm <- auto_commit(package = "git2rdata", repo = connection, push = TRUE),
+  "could not find repository from"
+)
+expect_identical(cm, git2r::last_commit(repo))
+
+write_vc(test_data, file = "test1", root = repo, stage = TRUE)
+file.remove(
+  list.files(connection, recursive = TRUE, full.names = TRUE, all.files = TRUE)
+)
+expect_error(
+  auto_commit(package = "git2rdata", repo = repo),
+  "Invalid repository"
+)
