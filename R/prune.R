3b6ecd7615ffd776f955358d9ae48a717ccc719e
ThierryO <thierry.onkelinx@inbo.be>
2019-02-04 17:54:40 +0100

    remove data on git more careful and rename the functions

--- R/prune.R
+++ R/prune.R
@@ -1,6 +1,6 @@
-#' Prune tsv files
+#' Remove data files
 #'
-#' Removes all tsv files from the `path` when they have an accompanying yml file
+#' Removes all data (`.tsv` files) from the `path` when they have accompanying metadata (`.yml` file). The metadata remains untouched.
 #' @param path the directory in which to clean all the data files
 #' @param recursive remove files in subdirectories too
 #' @return returns invisibily a vector of removed files names. The paths are
@@ -8,14 +8,14 @@
 #' @inheritParams write_vc
 #' @export
 #' @family storage
-prune_tsv <- function(
+rm_data <- function(
   root = ".", path = NULL, recursive = TRUE, ...
 ){
-  UseMethod("prune_tsv", root)
+  UseMethod("rm_data", root)
 }
 
 #' @export
-prune_tsv.default <- function(
+rm_data.default <- function(
   root, path = NULL, recursive = TRUE, ...
 ){
   stop("a 'root' of class ", class(root), " is not supported")
@@ -23,7 +23,7 @@ prune_tsv.default <- function(
 
 #' @export
 #' @importFrom assertthat assert_that is.flag
-prune_tsv.character <- function(
+rm_data.character <- function(
   root = ".", path = NULL, recursive = TRUE, ...
 ){
   assert_that(is.string(root))
@@ -58,40 +58,74 @@ prune_tsv.character <- function(
 
 #' @export
 #' @importFrom assertthat assert_that is.flag
-#' @importFrom git2r add
+#' @importFrom git2r workdir add
 #' @include write_vc.R
 #' @param stage stage the changes after removing the files. Defaults to FALSE.
-#' @rdname prune_tsv
-prune_tsv.git_repository <- function(
-  root, path = NULL, recursive = TRUE, ..., stage = FALSE
+#' @param type which classes of files should be removed. `unmodified` are files in the git history and unchanged since the last commit. `modified` are files in the git history and changed since the last commit. `ignored` refers to file listed in a `.gitignore` file. Selecting `modified` will remove both `unmodified` and `modified` data files. Selecting `ìgnored` will remove `unmodified`, `modified` and `ignored` data files. `all` refers to all visible data files, inclusing `untracked` files. The argument can be abbreviated to the first letter.
+#' @rdname rm_data
+rm_data.git_repository <- function(
+  root, path = NULL, recursive = TRUE, ..., stage = FALSE,
+  type = c("unmodified", "modified", "ignored", "all")
 ){
+  assert_that(is.string(path))
   assert_that(is.flag(stage))
-  removed <- prune_tsv(
-    root = workdir(root), path = path, recursive = recursive, ...
-  )
-  if (stage && !is.null(removed)) {
-    add(repo = root, path = removed)
+  type <- match.arg(type)
+  path <- file.path(workdir(root), path)
+  path <- normalizePath(path, winslash = "/", mustWork = FALSE)
+  if (!dir.exists(path)) {
+    return(invisible(NULL))
   }
-  return(invisible(removed))
+
+  to_do <- list.files(
+    path, pattern = "\\.tsv$", recursive = recursive, full.names = TRUE
+  )
+  yml <- list.files(
+    path, pattern = "\\.yml$", recursive = recursive, full.names = TRUE
+  )
+  yml <- gsub("\\.yml$", ".tsv", yml)
+  to_do <- to_do[to_do %in% yml]
+
+  keep <- unlist(switch(type,
+    unmodified = status(
+      root, staged = TRUE, unstaged = TRUE, untracked = TRUE, ignored = TRUE
+    ),
+    modified = status(
+      root, staged = FALSE, unstaged = FALSE, untracked = TRUE, ignored = TRUE
+    ),
+    ignored = status(
+      root, staged = FALSE, unstaged = FALSE, untracked = TRUE, ignored = FALSE
+    ),
+    all = list()
+  ))
+  to_do <- to_do[!to_do %in% file.path(workdir(root), keep)]
+  if (length(to_do) == 0) {
+    return(invisible(NULL))
+  }
+  file.remove(to_do)
+  to_do <- gsub(sprintf("^%s/(.*)$", workdir(root)), "\\1", to_do)
+  if (stage) {
+    add(repo = root, path = to_do)
+  }
+  return(invisible(to_do))
 }
 
-#' Prune yml files
+#' Prune metadata files
 #'
-#' Removes all yml files from the `path` when they don't have an accompanying tsv file
-#' @inheritParams prune_tsv
+#' Removes all metadata (`.yml` files) from the `path` when they don't have accompanying data (`.tsv` file).
+#' @inheritParams rm_data
 #' @return returns invisibily a vector of removed files names. The paths are
 #' relative to `root`.
 #' @inheritParams write_vc
 #' @export
 #' @family storage
-prune_yml <- function(
+prune_meta <- function(
   root = ".", path = NULL, recursive = TRUE, ...
 ){
-  UseMethod("prune_yml", root)
+  UseMethod("prune_meta", root)
 }
 
 #' @export
-prune_yml.default <- function(
+prune_meta.default <- function(
   root, path = NULL, recursive = TRUE, ...
 ){
   stop("a 'root' of class ", class(root), " is not supported")
@@ -99,7 +133,7 @@ prune_yml.default <- function(
 
 #' @export
 #' @importFrom assertthat assert_that is.flag
-prune_yml.character <- function(
+prune_meta.character <- function(
   root = ".", path = NULL, recursive = TRUE, ...
 ){
   assert_that(is.string(root))
@@ -134,19 +168,62 @@ prune_yml.character <- function(
 
 #' @export
 #' @importFrom assertthat assert_that is.flag
-#' @importFrom git2r add
+#' @importFrom git2r workdir add
 #' @include write_vc.R
 #' @param stage stage the changes after removing the files. Defaults to FALSE.
-#' @rdname prune_yml
-prune_yml.git_repository <- function(
+#' @rdname prune_meta
+prune_meta.git_repository <- function(
   root, path = NULL, recursive = TRUE, ..., stage = FALSE
 ){
-  assert_that(is.flag(stage))
-  removed <- prune_yml(
-    root = workdir(root), path = path, recursive = recursive, ...
-  )
-  if (stage && !is.null(removed)) {
-    add(repo = root, path = removed)
+  assert_that(is.string(path))
+  path <- file.path(workdir(root), path)
+  path <- normalizePath(path, winslash = "/", mustWork = FALSE)
+  if (!dir.exists(path)) {
+    return(invisible(NULL))
   }
-  return(invisible(removed))
+  assert_that(is.flag(recursive))
+  assert_that(is.flag(stage))
+
+  to_do <- list.files(
+    path = path,
+    pattern = "\\.yml$",
+    recursive = recursive,
+    full.names = TRUE
+  )
+  keep <- list.files(
+    path = path,
+    pattern = "\\.tsv$",
+    recursive = recursive,
+    full.names = TRUE
+  )
+  keep <- gsub("\\.tsv$", ".yml", keep)
+  to_do <- to_do[!to_do %in% keep]
+  if (length(to_do) == 0) {
+    return(invisible(NULL))
+  }
+
+  if (stage) {
+    changed <- unlist(status(
+      root, staged = FALSE, unstaged = TRUE, untracked = FALSE, ignored = FALSE
+    ))
+    changed <- gsub("\\.tsv$", ".yml", file.path(workdir(root), changed))
+    if (any(to_do %in% changed)) {
+      stop("cannot remove and stage metadata when data is removed but unstaged")
+    }
+  } else {
+    changed <- unlist(status(
+      root, staged = TRUE, unstaged = FALSE, untracked = FALSE, ignored = FALSE
+    ))
+    changed <- gsub("\\.tsv$", ".yml", file.path(workdir(root), changed))
+    if (any(to_do %in% changed)) {
+      warning("data removed and staged, metadata removed but unstaged")
+    }
+  }
+  file.remove(to_do)
+  to_do <- gsub(sprintf("^%s/(.*)$", workdir(root)), "\\1", to_do)
+
+  if (stage) {
+    add(repo = root, path = to_do)
+  }
+  return(invisible(to_do))
 }
--- tests/testthat/test_b_prune.R
+++ tests/testthat/test_b_prune.R
@@ -1,14 +1,14 @@
-context("prune_tsv & prune_yml")
+context("rm_data & prune_meta")
 
-expect_error(prune_tsv(root = 1), "a 'root' of class numeric is not supported")
-expect_error(prune_yml(root = 1), "a 'root' of class numeric is not supported")
+expect_error(rm_data(root = 1), "a 'root' of class numeric is not supported")
+expect_error(prune_meta(root = 1), "a 'root' of class numeric is not supported")
 
 root <- tempfile(pattern = "git2rdata-")
-expect_error(prune_tsv(root), root)
-expect_error(prune_yml(root), root)
+expect_error(rm_data(root), root)
+expect_error(prune_meta(root), root)
 dir.create(root)
-expect_null(prune_tsv(root, path = "junk"))
-expect_null(prune_yml(root, path = "junk"))
+expect_null(rm_data(root, path = "junk"))
+expect_null(prune_meta(root, path = "junk"))
 write_vc(test_data, file = "test", root = root, sorting = "test_Date")
 write_vc(
   test_data, file = "a/verbose", root = root, sorting = "test_Date",
@@ -16,14 +16,14 @@ write_vc(
 )
 
 current <- list.files(root, recursive = TRUE)
-expect_identical(prune_tsv(root = root, path = "a"), "a/verbose.tsv")
+expect_identical(rm_data(root = root, path = "a"), "a/verbose.tsv")
 expect_identical(
   list.files(root, recursive = TRUE),
   current[-grep("^.*/.*\\.tsv", current)]
 )
 
 current <- list.files(root, recursive = TRUE)
-expect_identical(prune_yml(root = root, path = "."), "a/verbose.yml")
+expect_identical(prune_meta(root = root, path = "."), "a/verbose.yml")
 expect_identical(
   list.files(root, recursive = TRUE),
   current[-grep("^.*/.*", current)]
@@ -31,7 +31,7 @@ expect_identical(
 
 file.remove(file.path(root, "test.yml"))
 current <- list.files(root, recursive = TRUE)
-expect_identical(prune_tsv(root, path = "."), character(0))
+expect_identical(rm_data(root, path = "."), character(0))
 expect_identical(list.files(root, recursive = TRUE), current)
 file.remove(file.path(root, "test.tsv"))
 
@@ -39,8 +39,8 @@ old_wd <- getwd()
 setwd(tempdir(check = TRUE))
 write_vc(test_data, file = "test", sorting = "test_Date")
 current <- list.files(".", recursive = TRUE, pattern = c("\\.(tsv|yml)$"))
-expect_identical(prune_tsv(path = ".", recursive = FALSE), "test.tsv")
-expect_identical(prune_yml(path = ".", recursive = FALSE), "test.yml")
+expect_identical(rm_data(path = ".", recursive = FALSE), "test.tsv")
+expect_identical(prune_meta(path = ".", recursive = FALSE), "test.yml")
 expect_identical(
   list.files(".", recursive = TRUE),
   character(0)
