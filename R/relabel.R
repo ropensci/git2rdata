42c2c0e8723c1435afc4fc9d523bdcc898e03992
ThierryO <thierry.onkelinx@inbo.be>
2019-02-21 11:47:01 +0100

    add relabel() to change factor labels in the metadata

---
+++ R/relabel.R
@@ -0,0 +1,76 @@
+#' Relabel factor levels
+#'
+#' Imaging the situation where we have a dataframe with a factor variable and we
+#' have stored it with `write_vc(optimize = TRUE)`. The raw data file contains
+#' the factor indices and the metadata contains the link between the factor
+#' index and the corresponding label.
+#' @inheritParams write_vc
+#' @param change a named list with named vectors. The name of list elements must
+#' match the names of the variables. The names of the vector elements must match
+#' the existing factor labels. The values represent the new factor labels
+#' @return invisible `NULL`
+#' @export
+#' @examples
+#' root <- tempfile("git2rdata-relabel")
+#' dir.create(root)
+#' ds <- ds <- data.frame(a = c("a1", "a2"), b = c("b2", "b1"))
+#' write_vc(ds, "relabel", root, sorting = "b")
+#' new_labels <- list(
+#'   a = list(a2 = "a3")
+#' )
+#' relabel("relabel", root, new_labels)
+relabel <- function(file, root, change) {
+  UseMethod("relabel", change)
+}
+
+#' @export
+relabel.default <- function(file, root, change) {
+  stop("a 'change' of class ", class(change), " is not supported")
+}
+
+#' @export
+#' @importFrom git2r workdir hash
+#' @importFrom assertthat assert_that is.string has_name
+#' @importFrom yaml read_yaml write_yaml
+relabel.list <- function(file, root, change) {
+  if (inherits(root, "git_repository")) {
+    return(relabel(file = file, root = workdir(root), change = change))
+  }
+  assert_that(is.string(root), is.string(file))
+  assert_that(!is.null(names(change)), msg = "'change' must be named")
+  root <- normalizePath(root, winslash = "/", mustWork = TRUE)
+  file <- clean_data_path(root = root, file = file)
+  assert_that(
+    all(file.exists(file)),
+    msg = "raw file and/or meta file missing"
+  )
+  meta_data <- read_yaml(file["meta_file"])
+  assert_that(has_name(meta_data, "..generic"))
+  optimize <- meta_data[["..generic"]][["optimize"]]
+  if (!optimize) {
+    stop("relabeling factors on verbose data leads to large diffs.
+Use write_vc() instead.")
+  }
+  assert_that(
+    all(names(change) %in% names(meta_data)),
+    msg = "every name in 'change' must match an exisiting variable"
+  )
+  for (id in names(change)) {
+    assert_that(
+      all(names(change[[id]]) %in% meta_data[[id]][["labels"]]),
+      msg = sprintf("the names in '%s' don't match existing labels", id)
+    )
+    names(meta_data[[id]][["labels"]]) <- meta_data[[id]][["labels"]]
+    meta_data[[id]][["labels"]][names(change[[id]])] <- change[[id]]
+    meta_data[[id]][["labels"]] <- unname(meta_data[[id]][["labels"]])
+    assert_that(
+      anyDuplicated(meta_data[[id]][["labels"]]) == 0,
+      msg = sprintf("relabeling '%s' leads to duplicated labels", id)
+    )
+  }
+  meta_data[["..generic"]][["hash"]] <- NULL
+  meta_data[["..generic"]] <- c(meta_data[["..generic"]],
+                                hash = hash(as.yaml(meta_data)))
+  write_yaml(meta_data, file["meta_file"])
+  return(invisible(NULL))
+}
---
+++ tests/testthat/test_d_relabel.R
@@ -0,0 +1,60 @@
+context("relabel")
+root <- tempfile("git2rdata-relabel")
+dir.create(root)
+ds <- data.frame(a = c("a1", "a2"), b = c("b2", "b1"))
+write_vc(ds, "relabel", root, sorting = "b")
+new_labels <- list(a = list(a2 = "a3"))
+test_that("relabel handles a list of changes", {
+  meta1 <- yaml::read_yaml(file.path(root, "relabel.yml"))
+  expect_null(relabel("relabel", root, new_labels))
+  meta2 <- yaml::read_yaml(file.path(root, "relabel.yml"))
+  expect_true(all(new_labels[["a"]] %in% meta2[["a"]][["labels"]]))
+  expect_true(all(
+    names(new_labels[["a"]]) %in%
+    meta1[["a"]][["labels"]][
+      !meta1[["a"]][["labels"]] %in% meta2[["a"]][["labels"]]
+    ]
+  ))
+  c_1 <- meta1
+  c_1[["a"]] <- NULL
+  c_1[["..generic"]][["hash"]] <- NULL
+  c_2 <- meta2
+  c_2[["a"]] <- NULL
+  c_2[["..generic"]][["hash"]] <- NULL
+  expect_identical(c_1, c_2)
+})
+
+test_that("relabel only works on optimized files", {
+  write_vc(ds, "relabel_verbose", root, sorting = "b", optimize = FALSE)
+  expect_error(relabel("relabel_verbose", root, new_labels),
+               "relabeling factors on verbose data leads to large diffs")
+})
+
+test_that("relabel handles git repositories", {
+  repo <- git2r::init(root)
+  git2r::config(repo, user.name = "Alice", user.email = "alice@example.org")
+  write_vc(ds, "relabel_git", repo, sorting = "b")
+  meta1 <- yaml::read_yaml(file.path(root, "relabel_git.yml"))
+  new_labels <- list(a = list(a2 = "a3"))
+  expect_null(relabel("relabel_git", repo, new_labels))
+  meta2 <- yaml::read_yaml(file.path(root, "relabel_git.yml"))
+  expect_true(all(new_labels[["a"]] %in% meta2[["a"]][["labels"]]))
+  expect_true(all(
+    names(new_labels[["a"]]) %in%
+    meta1[["a"]][["labels"]][
+      !meta1[["a"]][["labels"]] %in% meta2[["a"]][["labels"]]
+    ]
+  ))
+  c_1 <- meta1
+  c_1[["a"]] <- NULL
+  c_1[["..generic"]][["hash"]] <- NULL
+  c_2 <- meta2
+  c_2[["a"]] <- NULL
+  c_2[["..generic"]][["hash"]] <- NULL
+  expect_identical(c_1, c_2)
+})
+
+test_that("relabel returns an error for unknown classes", {
+  expect_error(relabel(change = NULL),
+               "a 'change' of class NULL is not supported")
+})
