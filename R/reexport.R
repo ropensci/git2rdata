88272bc638fa1ead79e01e58675f0a52fba7bb12
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2019-05-07 14:27:37 +0200

    make clear in the documentation of re-exported function that the documentation point to the originating package

--- R/reexport.R
+++ R/reexport.R
@@ -1,33 +1,33 @@
 #' Reexported function from git2r
 #'
-#' See \code{\link[git2r]{repository}}
+#' See \code{\link[git2r]{repository}} in `git2r`
 #' @name repository
 #' @importFrom git2r repository
 #' @family version_control
 #' @export
 NULL
 
-#' Reexported function from git2r
+#' Reexported function from `git2r`
 #'
-#' See \code{\link[git2r]{status}}
+#' See \code{\link[git2r]{status}} in `git2r`
 #' @name status
 #' @importFrom git2r status
 #' @family version_control
 #' @export
 NULL
 
-#' Reexported function from git2r
+#' Reexported function from `git2r`
 #'
-#' See \code{\link[git2r]{commit}}
+#' See \code{\link[git2r]{commit}} in `git2r`
 #' @name commit
 #' @importFrom git2r commit
 #' @family version_control
 #' @export
 NULL
 
-#' Reexported function from git2r
+#' Reexported function from `git2r`
 #'
-#' See \code{\link[git2r]{pull}}
+#' See \code{\link[git2r]{pull}} in `git2r`
 #' @name pull
 #' @importFrom git2r pull
 #' @family version_control
