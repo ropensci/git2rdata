34ea931c73e9e36b27de577bc334cf8c8d8f7662
ElsLommelen <els.lommelen@inbo.be>
2019-09-09 14:52:21 +0200

    Update R/datahash.R
    Co-Authored-By: Thierry Onkelinx <ThierryO@users.noreply.github.com>

--- R/datahash.R
+++ R/datahash.R
@@ -1,6 +1,6 @@
 #' Calculate Hash of Dataframe
 #' Calculates a hash based on the given data that is compatible with different operating systems.
-#' @param data dataframe to calculate the hash
+#' @param data Dataframe to calculate the hash.
 #' @param convert convert characters in dataframe data to UTF-8?
 #' @return A 40 digit unique code representing the dataframe
 #' @noRd
