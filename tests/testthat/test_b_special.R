ab35a8d092b1b7a0c00384a86f936789b5882167
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2019-10-25 14:26:51 +0200

    hash problem might be due to differences in factor levels
    sorting depends on the locale

--- tests/testthat/test_b_special.R
+++ tests/testthat/test_b_special.R
@@ -71,6 +71,20 @@ expect_equal(
   "1d4ff22735dc3d7e7f94386e149f0acde22e8c3b"
 )
 
+expect_identical(
+  as.integer(ds$a),
+  c(15L, 23L, 18L, 19L, 1L, 17L, 21L, 22L, 2L, 20L, 29L, 30L, 6L,
+28L, 7L, 26L, 27L, 3L, 25L, 4L, 24L, 8L, 5L, NA, 32L, 10L, 16L,
+33L, 31L, 14L, 12L, 11L, 9L, 13L)
+)
+expect_identical(
+  levels(ds$a),
+  c("\ta", "\na", "'b", "'b'", "'NA'", "\"b", "\"b\"", "\"NA\"",
+"@", "&", "#", "|", "$", "€", "a", "\U00E0", "a\t", "a\tb", "a\tb\tc",
+"a\n", "a\nb", "a\nb\nc", "a b", "a b c", "a'", "a'b", "a'b'c",
+"a\"", "a\"b", "a\"b\"c", "\U00E7", "\U00E9", "\U00B5")
+)
+
 expect_equal(
   names(suppressWarnings(write_vc(ds, "test_data_hash", root)))[1],
   "dd99251ec3ad80ce137db113a8baee5801e73bc4"
@@ -93,7 +107,10 @@ yaml::write_yaml(yaml_file, file.path(root, "test_data_hash.yml"))
 expect_warning(read_vc("test_data_hash", root = root),
              "Mismatching data hash. Data altered outside of git2rdata.")
 
-
-
+ds$a <- as.character(ds$a)
+expect_equal(
+  names(suppressWarnings(write_vc(ds, "test_data_hash2", root)))[1],
+  "a5bdb23c869ada2eedffa7add5c18dca38aa3d61"
+)
 
 file.remove(list.files(root, recursive = TRUE, full.names = TRUE))
