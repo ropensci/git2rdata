1e16f28f29515bc634ac2567c9ca0613cb501742
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2019-11-18 13:59:41 +0100

    exclude release_questions() from code coverage

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,7 @@
+# function used by devtools::release()
 # gramr is avaible from https://github.com/ropenscilabs/gramr
-release_questions <- function() {
+release_questions <- function() { # nocov start
   c(
     'Did you ran `gramr::check_project(exclude_chunks = TRUE)`'
   )
-}
+} # nocov end
