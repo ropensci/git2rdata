52526d2018e8fa4ef42504c233f28f931554c853
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2022-02-25 15:51:55 +0100

    add unit test for verify_vc()

---
+++ tests/testthat/test_b_verify_vc.R
@@ -0,0 +1,28 @@
+test_that("verify_vc", {
+  root <- tempfile(pattern = "git2rdata-verify-vc")
+  dir.create(root)
+  write_vc(
+    x = test_data, file = "test.txt", root = root, sorting = "test_integer"
+  )
+  expect_s3_class(
+    verify_vc("test.txt", root = root, variables = "test_integer"),
+    "data.frame"
+  )
+  expect_s3_class(
+    verify_vc(
+      "test.txt", root = root, variables = c("test_numeric", "test_logical")
+    ),
+    "data.frame"
+  )
+  expect_error(
+    verify_vc("test.txt", root = root, variables = c("test_integer", "junk")),
+    "variables missing.*junk"
+  )
+
+  file.remove(
+    list.files(root, recursive = TRUE, full.names = TRUE)
+  )
+  file.remove(
+    list.files(root, recursive = TRUE, include.dirs = TRUE, full.names = TRUE)
+  )
+})
