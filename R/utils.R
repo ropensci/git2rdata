a46180cbdd59c51467827c34ca3d47cfe35b92d1
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2024-08-28 11:35:00 +0200

    the gramr is no longer available

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,3 @@
-# function used by devtools::release()
-# gramr is avaible from https://github.com/ropenscilabs/gramr
-release_questions <- function() { # nocov start
-  c(
-    "Did you ran `gramr::check_project(exclude_chunks = TRUE)`"
-  )
-} # nocov end
-
 #' @noRd
 #' @importFrom utils flush.console
 #' @importFrom assertthat assert_that is.flag noNA
